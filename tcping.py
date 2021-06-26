87d295fa327f13242b38d411a413b7f15281815a
yu-n0z <lifiers@gmail.com>
2021-06-26 23:30:49 +0900

    ctrl+c 押下時の処理を追加

--- tcping.py
+++ tcping.py
@@ -1,6 +1,7 @@
-from os import truncate
 import socket
 import sys
+import time
+import datetime
 
 # ipアドレスを取得
 PCIP = socket.gethostbyname(socket.gethostname())
@@ -10,11 +11,13 @@ InFlg = 0
 #### オプションの設定
 try:
     while True:
+        #サーバーモードかクライアントモードを洗濯
         if InFlg == 0:
             mode =input("mode? [1:Server or 2:Clients]:")
             if mode == '1' or mode == '2':
                 InFlg += 1
 
+        #ソースIPを指定、表示されたIPのままでOKならEnter
         if InFlg == 1:
             Sip=input(f"Source IP [{PCIP}]? or type:")
             if Sip == "":
@@ -24,6 +27,7 @@ try:
                 Src_ip = Sip
                 InFlg += 1
         
+        #送信元ポートを指定
         if InFlg == 2:
             x = input("Source Port?[1-65536]:")
             if x != "":
@@ -32,6 +36,7 @@ try:
                     S_port = str(sp)
                     InFlg += 1
         
+        #宛先IPを入力する
         if InFlg == 3 and mode == "2":
             dip =input("Destination IP?")
             if dip != "":
@@ -42,6 +47,7 @@ try:
             D_port = ""
             break
 
+        #宛先ポートを指定
         if InFlg == 4 and mode == "2":
             x=input("Destination Port?[1-65536]:")
             if x != "":
@@ -57,6 +63,8 @@ except KeyboardInterrupt:
     sys.exit()
 
 # print(f"送信元IP{Src_ip}\n送信元ポート{S_port}\n宛先IP{Dest_ip}\n宛先ポート{D_port}")
+
+#Clientモードの処理
 if mode == "2":
     init_msg = "Sourte_IP:" + Src_ip +  "\nSource_Port:" + S_port + "\nDest_IP:" + Dest_ip + "\nDest_port:" + D_port
 
@@ -66,19 +74,39 @@ if mode == "2":
     s.send(bytes(init_msg,'utf-8'))
     response = s.recv(1024)
     #print("[*]Received a response : {}".format(response))
-    print(response)
+    print(response.decode('utf-8'))
     
-    if response == bytes("ack",'utf-8'):
-        while True:
-        
-            x =input("press any key:")
-            if x != "" and x != "bye":
-                s.send(bytes(x,'utf-8'))
-            elif x == "bye":
-                s.send(bytes(x,'utf-8'))
-                break
+    if response.decode('utf-8') == "ack":
+        try:
+            while True:
+            
+                x =input(">>")
+                if x != "" and x != "bye" and x != "ping":
+                    s.send(bytes(x,'utf-8'))
+                elif x == "bye":
+                    s.send(bytes(x,'utf-8'))
+                    break
+                elif x == "ping":
+                    try:
+                        while True:
+                            #送信メッセージにタイムスタンプを付与する
+                            now = datetime.datetime.now()
+                            Send_MSG = "TCP recieved " + str(now.time())
+                            s.send(bytes(Send_MSG,'utf-8'))
 
+                            response = s.recv(1024)
+                            print(response.decode('utf-8'))
+
+                            time.sleep(1)
+                    except KeyboardInterrupt:
+                        pass
+
+        except KeyboardInterrupt:
+            s.send(bytes("bye",'utf-8'))
+
+#サーバーモードの処理
 if mode == "1":
+    init_msg = "Source_IP:" + Src_ip + "\nSource_port:" + S_port
     ServerFlg = 0
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((Src_ip,int(S_port)))  # IPとポート番号を指定します
@@ -91,15 +119,28 @@ if mode == "1":
 
         client_data = clientsocket.recv(1024)
         print(client_data.decode('utf-8'))
-        
+
         while True:
             client_data = clientsocket.recv(1024)
-            print(f">>",client_data.decode('utf-8'))
+            decode_data = client_data.decode('utf-8')
 
-            if client_data.decode('utf-8') == "bye":
+            #メッセージモードの処理
+            if decode_data != "" and decode_data[:3] != "TCP":
+                print(f">>",decode_data)
+
+            #ping メッセージがきたらReplyを返す
+            if decode_data[:3] == "TCP":
+                print(decode_data)
+
+                now = datetime.datetime.now()
+                Reply_MSG = "TCP Reply " + str(now.time())
+                clientsocket.send(bytes(Reply_MSG,'utf-8'))
+
+            #bye メッセージがきたら終了
+            if decode_data == "bye":
+                print("ClientからByeが入力されました")
                 clientsocket.close()
                 ServerFlg = "1"
                 break
         if ServerFlg == "1":
             break
-
